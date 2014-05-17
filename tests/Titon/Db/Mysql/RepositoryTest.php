306daf58490651705d7f3dce4a58a028c0da100f
Miles Johnson <mileswjohnson@gmail.com>
2014-05-16 21:34:22 -0700

    Fix broken test

--- tests/Titon/Db/Mysql/RepositoryTest.php
+++ tests/Titon/Db/Mysql/RepositoryTest.php
@@ -3,4 +3,11 @@ namespace Titon\Db\Mysql;
 
 class RepositoryTest extends \Titon\Db\RepositoryTest {
 
+    public function testSelect() {
+        $query = new MysqlQuery(MysqlQuery::SELECT, $this->object);
+        $query->from($this->object->getTable(), 'User')->fields('id', 'username');
+
+        $this->assertEquals($query, $this->object->select('id', 'username'));
+    }
+
 }
